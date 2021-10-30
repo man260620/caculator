510af00e365bc611cc0ded6879f30f082109d335
man260620 <tranthiman26062000@gmail.com>
2021-10-30 23:39:50 +0700

    update form

--- caculator/Form1.Designer.cs
+++ caculator/Form1.Designer.cs
@@ -29,13 +29,100 @@ namespace caculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtsoA = new System.Windows.Forms.TextBox();
+            this.txtsob = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "số A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "số B";
+            // 
+            // txtsoA
+            // 
+            this.txtsoA.Location = new System.Drawing.Point(166, 41);
+            this.txtsoA.Name = "txtsoA";
+            this.txtsoA.Size = new System.Drawing.Size(156, 22);
+            this.txtsoA.TabIndex = 2;
+            // 
+            // txtsob
+            // 
+            this.txtsob.Location = new System.Drawing.Point(166, 94);
+            this.txtsob.Name = "txtsob";
+            this.txtsob.Size = new System.Drawing.Size(156, 22);
+            this.txtsob.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "kq";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 59);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "cộng";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(166, 149);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(156, 22);
+            this.txtkq.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(507, 362);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtsob);
+            this.Controls.Add(this.txtsoA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "số B";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtsoA;
+        private System.Windows.Forms.TextBox txtsob;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtkq;
     }
 }
 
--- caculator/Form1.cs
+++ caculator/Form1.cs
@@ -16,5 +16,7 @@ namespace caculator
         {
             InitializeComponent();
         }
+
+      
     }
 }
